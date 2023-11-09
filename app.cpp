6723e21d19642e4527efdf93729c551adffb27ed
axelaquino3 <axelaquino@google.com>
2023-11-09 17:05:22 -0500

    Files added

---
+++ app.cpp
@@ -0,0 +1,35 @@
+// AXEL'S CODE
+
+// #include <iostream>
+// #include "profile.hpp"
+
+
+
+// int main() {
+
+//   // instantiate
+//   Profile sam("Sam Drakilla", 30, "New York", "USA", "he/him");
+
+//   std::cout << "hello";
+
+// }
+
+
+
+
+
+
+// CODECADEMY'S SOLUTION CODE
+#include <iostream>
+#include "profile.hpp"
+
+int main() {
+
+  Profile sam("Sam Drakkila", 30, "New York", "USA", "he/him");
+//   sam.add_hobby("listening to audiobooks and podcasts");
+//   sam.add_hobby("playing rec sports like bowling and kickball");
+//   sam.add_hobby("writing a speculative fiction novel");
+//   sam.add_hobby("reading advice columns");
+    sam.view_profile();
+
+}
---
+++ profile.hpp
@@ -0,0 +1,46 @@
+// AXEL'S CODE!!!!!
+
+// #include <vector>
+
+// class Profile {
+// //   private:
+//   // Attributes
+//     std::string name;
+//     int age;
+//     std::string city;
+//     std::string country;
+//     std::string pronouns;
+//     std::vector<std::string> hobbies;
+
+
+//   public:
+//     // Constructor for Profile
+//     Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns = "they/them");
+
+//     //Declare Methods
+//     std::string view_profile();
+
+// };
+
+
+// CODECADEMY SOLUTIONS CODE!!!!
+
+#include <vector>
+#include <string>
+
+class Profile {
+private:
+  std::string name;
+  int age;
+  std::string city;
+  std::string country;
+  std::string pronouns;
+  std::vector<std::string> hobbies;
+  
+public:
+  Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns);
+  
+  std::string view_profile();
+  void add_hobby(std::string new_hobby);
+
+};
